426844d513cebca803eb6531ef336a999f8c052e
roberfernandez <62353267+roberfernandez@users.noreply.github.com>
2026-09-24 11:53:54 +0200

    Actualiza caché PWA para selector v10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="cinta-metrica-v9";
+const CACHE="cinta-metrica-v10";
 const ASSETS=["./","./index.html","./style.css?v=9","./app.js?v=9","./manifest.webmanifest"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
